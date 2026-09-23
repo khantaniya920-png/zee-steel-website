b35a7456dd4ea0ff5c653953571d06ec856c3a18
TANIYA KHAN <KHANTANIYA920@GMAIL.COM>
2026-09-23 21:38:04 +0530

    Add residential industrial mall and public project images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,6 +8,21 @@ import Arc1 from '../assets/images/Arc1.png'
 import Arc2 from '../assets/images/Arc2.png'
 import Arc3 from '../assets/images/Arc3.png'
 import Arc4 from '../assets/images/Arc4.png'
+import Lux_Gate1 from '../assets/images/Lux_Gate1.png'
+import Lux_Gate2 from '../assets/images/Lux_Gate2.png'
+import Lux_Gate3 from '../assets/images/Lux_Gate3.png'
+import Lux_Gate4 from '../assets/images/Lux_Gate4.png'
+import Lux_Gate5 from '../assets/images/Lux_Gate5.png'
+import Lux_Gate6 from '../assets/images/Lux_Gate6.png'
+import Industrial_Fab from '../assets/images/Industrial_Fab.png'
+import Tank1 from '../assets/images/Tank1.png'
+import Tank2 from '../assets/images/Tank2.png'
+
+import Mall_Kids_Zone from '../assets/images/Mall_Kids_Zone.png'
+import Mall_Lift1 from '../assets/images/Mall_Lift1.png'
+import Mall3 from '../assets/images/Mall3.png'
+
+import Govt_Public from '../assets/images/Govt & Public.png'
 
 const projectImages = {
   SCL1,
@@ -18,6 +33,19 @@ const projectImages = {
   Arc2,
   Arc3,
   Arc4,
+  Lux_Gate1,
+  Lux_Gate2,
+  Lux_Gate3,
+  Lux_Gate4,
+  Lux_Gate5,
+  Lux_Gate6,
+  Industrial_Fab,
+  Tank1,
+  Tank2,
+  Mall_Kids_Zone,
+  Mall_Lift1,
+  Mall3,
+  Govt_Public,
 }
 
 export default function ProjectCard({ project }) {
